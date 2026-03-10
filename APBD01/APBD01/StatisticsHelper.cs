a7840334efb49c076dae1bbc7f5bd82abe01b096
Tangonell <59707404+tangonell@users.noreply.github.com>
2026-03-10 16:57:41 +0100

    Create CalculateAverage method

--- APBD01/APBD01/StatisticsHelper.cs
+++ APBD01/APBD01/StatisticsHelper.cs
@@ -2,5 +2,13 @@
 
 public class StatisticsHelper
 {
-    
+    public static double CalculateAverage(int[] values)
+    {
+        double sum = 0;
+        foreach (var value in values)
+        {
+            sum += value;
+        }
+        return sum / values.Length;
+    }
 }
