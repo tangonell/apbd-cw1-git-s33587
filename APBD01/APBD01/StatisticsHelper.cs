b30ad2b004de62829d9c989efc5335e04835de6d
Tangonell <59707404+tangonell@users.noreply.github.com>
2026-03-16 18:23:44 +0100

    Add `CalculateMax` method

--- APBD01/APBD01/StatisticsHelper.cs
+++ APBD01/APBD01/StatisticsHelper.cs
@@ -12,4 +12,16 @@ public class StatisticsHelper
         var average = sum / values.Length;
         return average;
     }
+
+    public static int CalculateMax(int[] values)
+    {
+        if (values.Length == 0) return 0;
+        var max = values[0];
+        for (var i = 1; i < values.Length; i++)
+        {
+            if (values[i] > max)
+                max = values[i];
+        }
+        return max;
+    }
 }
