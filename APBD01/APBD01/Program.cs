513e6b08fcd9e84caff183d93099babfe102cd94
Tangonell <59707404+tangonell@users.noreply.github.com>
2026-03-16 17:54:44 +0100

    Add default username if the provided value is blank

--- APBD01/APBD01/Program.cs
+++ APBD01/APBD01/Program.cs
@@ -4,4 +4,5 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello, user! What is your name?");
 var name = Console.ReadLine();
+if (string.IsNullOrEmpty(name)) name = "anon";
 Console.WriteLine($"Nice to meet you, {name}!");
